2ecd510a258d3d51c9380b4f2107b8e720579f12
shinji0320 <shinji.blue.one@gmail.com>
2021-05-08 13:36:27 +0900

    配列の要素を変更する

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 "use strict";
 
-console.log("Hello World");
+{
+  const scores = [80, 90, 40, 70,];
+  
+  const [a, b, ...others] = scores;
+  console.log(a);
+  console.log(b);
+  console.log(others);
+}
+
+// const otherScore = [12, 14]
+// const scores = [80, 90, 40, 70, ...otherScore];
+
+  // for (let i = 0; i < scores.length; i++){
+  //   console.log(`Score ${i}: ${scores[i]}`);
+  // }
